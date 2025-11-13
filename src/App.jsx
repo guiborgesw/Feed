599ca33cee8ab85ff4b5db682363c79976292c67
Guilherme Borges <borgesg226@gmail.com>
2025-11-13 00:19:52 -0300

    Add Global CSS to my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './global.css';
 
  export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+
       <Post 
         author="Guilherme Borges" 
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem esse, et deserunt consequuntur explicabo omnis blanditiis obcaecati reprehenderit quibusdam error nesciunt eius officia eligendi dolores, aut accusantium mollitia quasi facere!" 

03b17c78bc1dcef0ddd129cae618e5ed81dcc999
Guilherme Borges <borgesg226@gmail.com>
2025-11-18 23:43:40 -0300

    Create the Sidebar Component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Capa de Perfil Desenvolvedor" 
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/guiborgesw.png" alt="Minha Foto de Perfil"/>
+
+                <strong>Guilherme Borges</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}

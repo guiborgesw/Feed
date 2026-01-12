1de386f2ff6269a20131cf34c7a87d8a8922fd5c
Guilherme Borges <borgesg226@gmail.com>
2026-01-12 09:28:01 -0300

    Create the Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+
+export function Comment({ content }) {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/guiborgesw.png"/>
+
+            <div className={styles.commentBox}> 
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Guilherme Borges</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2025-05-11 08:13:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}

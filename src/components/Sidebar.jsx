21dcab32c09a301431fcae56a7f0e88202dc6cb2
Guilherme Borges <borgesg226@gmail.com>
2026-01-12 09:34:34 -0300

    Automating images from Avatar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return(
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/guiborgesw.png" alt="Minha Foto de Perfil"/>
+                <Avatar src="https://github.com/guiborgesw.png"/>
 
                 <strong>Guilherme Borges</strong>
                 <span>Web Developer</span>
